03cf0e0cfca81685e4a12d6d60fe1e1f65601fdf
wrottger <wrottger@student.42wolfsburg.de>
2023-09-03 18:37:40 +0200

    added basic server logic

---
+++ src/server.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrottger <wrottger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 10:09:37 by wrottger          #+#    #+#             */
+/*   Updated: 2023/09/03 18:19:03 by wrottger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <unistd.h>
+#include <signal.h>
+#include "utils.h"
+
+
+void	*add_bit(char **msg, size_t msg_bit_len, size_t msg_len, int bit)
+{
+	if ((msg_bit_len % 8) == 0)
+	{
+		msg = ft_realloc(msg, msg_len, msg_len + 1);
+	}
+	set_bit(*msg, msg_bit_len, bit);
+	return (msg);
+}
+
+void	handle_sent_bit(int signal)
+{
+	static char		*msg = NULL;
+	static size_t	msg_bit_len = 0;
+	size_t			msg_len;
+
+	msg_len = msg_bit_len / sizeof(char);
+	if (signal == SIGUSR1)
+		add_bit(&msg, msg_bit_len, msg_len, 1);
+	if (signal == SIGUSR2)
+		add_bit(&msg, msg_bit_len, msg_len, 0);
+	if (msg[msg_len] == 0)
+	{
+		write(1, msg, msg_len);
+		free(msg);
+		msg = NULL;
+		msg_bit_len = 0;
+	}
+	msg_bit_len++;
+}
+
+int	main(void)
+{
+	ft_putnbr_fd(getpid(), 1);
+	signal(SIGUSR1, handle_sent_bit);
+	signal(SIGUSR2, handle_sent_bit);
+	while (1)
+		;
+	return (0);
+}
