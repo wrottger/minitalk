6e93f36bd1c706a14641836943a03be0f3a0cea6
wrottger <wrottger@student.42wolfsburg.de>
2023-09-03 20:22:39 +0200

    Fixed error with argument name colliding with function

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: wrottger <wrottger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 10:09:37 by wrottger          #+#    #+#             */
-/*   Updated: 2023/09/03 19:17:20 by wrottger         ###   ########.fr       */
+/*   Updated: 2023/09/03 20:21:57 by wrottger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include "utils.h"
+#include <printf.h>
 
 
 void	*add_bit(char **msg, size_t msg_bit_len, size_t msg_len, int bit)
@@ -23,19 +24,20 @@ void	*add_bit(char **msg, size_t msg_bit_len, size_t msg_len, int bit)
 		msg = ft_realloc(msg, msg_len, msg_len + 1);
 	}
 	set_bit(*msg, msg_bit_len, bit);
+	printf("%d", bit);
 	return (msg);
 }
 
-void	handle_sent_bit(int signal)
+void	handle_sent_bit(int sig)
 {
 	static char		*msg = NULL;
 	static size_t	msg_bit_len = 0;
 	size_t			msg_len;
 
 	msg_len = msg_bit_len / sizeof(char);
-	if (signal == SIGUSR1)
+	if (sig == SIGUSR1)
 		add_bit(&msg, msg_bit_len, msg_len, 1);
-	if (signal == SIGUSR2)
+	if (sig == SIGUSR2)
 		add_bit(&msg, msg_bit_len, msg_len, 0);
 	if (msg[msg_len] == 0)
 	{
